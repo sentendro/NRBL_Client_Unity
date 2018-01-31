204f95bdbb8d69df45aad4b327cb5406d84d674c
unknown <heokyunam@naver.com>
2018-01-31 23:31:53 +0900

    크기가 1이 되게끔 텍스쳐의 크기를 고려해 GameObject 크기를 조정함. 이를 위해 AutoSizer라는 클래스를 사용함. 하지만 Transform을 복사해서 넣어두었기에 더 이상 AutoSizer를 굳이 스크립트에 추가해둘 필요가 없음
    push by. heokyunam

---
+++ Assets/scripts/ui/AutoSizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 size = this.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        this.transform.localScale = new Vector3(1 / size.x, 1 / size.y);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
