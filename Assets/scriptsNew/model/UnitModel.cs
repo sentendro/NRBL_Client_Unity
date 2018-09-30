6b57474a8b06992aafc27d475179af1bb3a42eb8
heokyunam <heokyunam@naver.com>
2018-09-30 14:14:20 +0900

    UnitModel 생성자 그 과정에서 Util 추가

--- Assets/scriptsNew/model/UnitModel.cs
+++ Assets/scriptsNew/model/UnitModel.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Xml.Linq;
+using System;
 
 public class UnitModel
 {
-    //private int hp = 0, price = 0, gold = 0, capacity = 0, attack = 0;
+    //private int hp = 0, price = 0 , gold = 0, capacity = 0, attack = 0;
     //private bool movable = false;
 
     public int Hp { get; set; }
@@ -14,4 +13,29 @@ public class UnitModel
     public int Attack { get; set; }
 
     public bool Movable { get; set; }
+
+    public UnitModel appendix { get; set; } //10턴이 지나면 달라질 모습
+
+    public UnitModel(XElement data)
+    {
+        this.Hp = Util.ParseInt(data.Element("Hp"));
+        this.Price = Util.ParseInt(data.Element("Price"));
+        this.Gold = Util.ParseInt(data.Element("Gold"));
+        this.Capacity = Util.ParseInt(data.Element("Capacity"));
+        this.Attack = Util.ParseInt(data.Element("Attack"));
+
+        this.Movable = data.Element("Movable").Equals("true") ? true : false;
+
+        XElement xeAppendix = data.Element("Appendix");
+
+        //10턴이후 변경 사항 저장
+        if(xeAppendix != null)
+        {
+            appendix.Hp = Math.Max(Util.ParseInt(xeAppendix.Element("Hp")), this.Hp);
+            appendix.Price = Math.Max(Util.ParseInt(xeAppendix.Element("Price")), this.Price);
+            appendix.Gold = Math.Max(Util.ParseInt(xeAppendix.Element("Gold")), this.Gold);
+            appendix.Capacity = Math.Max(Util.ParseInt(xeAppendix.Element("Capacity")), this.Capacity);
+            appendix.Attack = Math.Max(Util.ParseInt(xeAppendix.Element("Attack")), this.Attack);
+        }
+    }
 }
---
+++ Assets/scriptsNew/tools/Util.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+public class Util
+{
+    public static int ParseInt(XElement xe)
+    {
+        if(xe == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return int.Parse(xe.Value);
+        }
+    }
+
+    public static bool ParseBool(XElement xe)
+    {
+        if(xe == null)
+        {
+            return false;
+        }
+        else
+        {
+            return xe.Equals("true") ? true : false;
+        }
+    }
+}
