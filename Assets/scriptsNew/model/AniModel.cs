9c4ceacae6546a3526496d372b199fd46d8a3ff0
heokyunam <heokyunam@naver.com>
2018-12-02 22:35:30 +0900

    애니메이션 작업. AniController에서 좀 더 세부작업이 필요 할듯

---
+++ Assets/scriptsNew/model/AniModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class AniModel
+{
+    public const int TYPE_MOVE = 1, TYPE_ATTACK = 2, TYPE_UPGRADE = 3, TYPE_DEATH = 4;
+    public int Type { get; set; }
+    public UnitModel Subject { get; set; }//subject to object(attack)
+    public UnitModel Object { get; set; }
+
+    public AniModel(int type, UnitModel sub)
+    {
+        this.Type = type;
+        this.Subject = sub;
+    }
+
+    public AniModel(int type, UnitModel sub, UnitModel obj)
+    {
+        this.Type = type;
+        this.Subject = sub;
+        this.Object = obj;
+    }
+}
