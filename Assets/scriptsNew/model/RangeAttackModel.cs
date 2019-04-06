a0b9924fd5e0087a7ce27834521a8d5514a9b53e
GitHub <heokyunam@naver.com>
2019-04-07 00:10:14 +0900

    프로젝트 구조자체를 변경할 필요성을 느낌
    기존에는 MVC 모델을 추구하려고 많이 노력했으나
    새로운 기능을 추가하는데 너무 제약이 많아 이를 포기

--- Assets/scriptsNew/model/RangeAttackModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RangeAttackModel {
-    public GameObject Prefab { get; set; }
-    public int Range { get; set; }
-    public int Attack { get; set; }
-}
