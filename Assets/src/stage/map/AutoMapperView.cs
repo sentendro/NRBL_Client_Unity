65e7ce982b245e278406c7cd6bc386dcdff38df0
GitHub <heokyunam@naver.com>
2019-04-07 11:34:18 +0900

    씬내에서 맵이랑 팔레트를 만들어 놓음

--- Assets/src/stage/map/AutoMapperView.cs
+++ Assets/src/stage/map/AutoMapperView.cs
@@ -20,7 +20,7 @@ public class AutoMapperView : MonoBehaviour {
         xmlData = xml.Split('\n');
         for (int i = 0; i < xmlData.Length; i++)
         {
-            string rowData = xmlData[i];
+            string rowData = xmlData[i].Trim();
 
             //row별로 묶어서 맵을 관리
             GameObject rowObj = new GameObject();
@@ -28,7 +28,7 @@ public class AutoMapperView : MonoBehaviour {
             rowObj.transform.localPosition = new Vector3(xDif, i + yDif, 0);
             rowObj.name = "row" + i;
 
-            for(int j = 0; j < rowData.Length - 1; j++)
+            for(int j = 0; j < rowData.Length; j++)
             {
                 int tileIdx = rowData[j] - '0';
                 GameObject tileObj = Instantiate(tiles[tileIdx], rowObj.transform);
