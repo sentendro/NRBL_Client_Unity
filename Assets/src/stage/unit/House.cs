9b0f82cd9cf78bc0d966c041532e430a53033272
GitHub <heokyunam@naver.com>
2019-04-20 20:21:10 +0900

    에너미 턴체킹 텍스트 표출 버그 수정

--- Assets/src/stage/unit/House.cs
+++ Assets/src/stage/unit/House.cs
@@ -9,14 +9,6 @@ public class House : Unit
     public bool outIsEnemy;
     public int outAddValue = 3;
 
-    private void Start()
-    {
-        if (outIsEnemy == true && outAiEnemy == null)
-        {
-            throw new NoAssignedException(this.GetType() + ".outAiEnemy");
-        }
-    }
-
     public override void OnCreate()
     {
         AIEnemy aiEnemy = GameResources.AIEnemy;
--- Assets/src/stage/unit/common/TurnChecker.cs
+++ Assets/src/stage/unit/common/TurnChecker.cs
@@ -8,17 +8,23 @@ public class TurnChecker : MonoBehaviour
     public GameObject outPrefab;
     public Text inText;
     public int turn;
+    public bool isEnemy;
     public bool isUpgraded = false;
 
     public void OnUnitCreate()
     {
-        GameObject obj = Instantiate(outPrefab, GameObject.Find("Canvas").transform);
-        obj.SetActive(true);
-        Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
-        position.y += 15;
-        obj.transform.position = position;
-        inText = obj.GetComponent<Text>();
-        inText.text = turn.ToString();
+        if (isEnemy == false)
+        {
+            // 텍스트 복사
+            GameObject obj = Instantiate(outPrefab, GameObject.Find("Canvas").transform);
+            obj.SetActive(true);
+            Vector3 position = Camera.main.WorldToScreenPoint(transform.position);
+            position.y += 15;
+            obj.transform.position = position;
+
+            inText = obj.GetComponent<Text>();
+            inText.text = turn.ToString();
+        }
     }
 
     public void TurnUpdate()
@@ -32,7 +38,10 @@ public class TurnChecker : MonoBehaviour
                 inText.color = Color.cyan;
             }
 
-            inText.text = turn.ToString();
+            if(isEnemy == false)
+            {
+                inText.text = turn.ToString();
+            }
         }
     }
 }
